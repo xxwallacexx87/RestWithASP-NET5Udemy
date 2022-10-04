59fcdbfe05db144ef6426495d606a92ef1146168
xxwallacexx87 <xxwallacexx_@hotmail.com>
2022-10-04 00:26:34 -0300

    Working with four verbs

---
+++ RestWithASP-NET5Udemy/RestWithASP-NET5Udemy/RestWithASP-NET5Udemy/Controllers/PersonController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace RestWithASP_NET5Udemy.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PersonController : ControllerBase
+    {
+        private readonly ILogger<PersonController> _logger;
+
+        public PersonController(ILogger<PersonController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("sum/{firstNumber}/{secondNumber}")]
+        public IActionResult Sum(string firstNumber, string SecondNumber)
+        {
+            return BadRequest("Invalid input");
+        }
+
+    }
+}
---
+++ RestWithASP-NET5Udemy/RestWithASP-NET5Udemy/RestWithASP-NET5Udemy/Model/Person.cs
@@ -0,0 +1,11 @@
+﻿namespace RestWithASP_NET5Udemy.Model
+{
+    public class Person
+    {
+        public long Id { get; set; }    
+        public long FirstName { get; set; }    
+        public long LastName { get; set; }    
+        public long Address { get; set; }    
+        public long Gender { get; set; }    
+    }
+}
